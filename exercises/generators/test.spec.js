e65aa08192404aa8cac96f1e1d55582ab8adeb44
Florian Orpelière <florian.orpeliere@gmail.com>
2017-04-19 00:05:25 +0200

    Add .value

--- exercises/generators/test.spec.js
+++ exercises/generators/test.spec.js
@@ -9,12 +9,12 @@ describe('Test exercise for classes', function () {
   });
 
   it('Fibonacci rocks', () => {
-    expect(instanceToTest.next()).toBe(1);
-    expect(instanceToTest.next()).toBe(2);
-    expect(instanceToTest.next()).toBe(3);
-    expect(instanceToTest.next()).toBe(5);
-    expect(instanceToTest.next()).toBe(8);
-    expect(instanceToTest.next()).toBe(13);
+    expect(instanceToTest.next().value).toBe(1);
+    expect(instanceToTest.next().value).toBe(2);
+    expect(instanceToTest.next().value).toBe(3);
+    expect(instanceToTest.next().value).toBe(5);
+    expect(instanceToTest.next().value).toBe(8);
+    expect(instanceToTest.next().value).toBe(13);
   });
 
 });
